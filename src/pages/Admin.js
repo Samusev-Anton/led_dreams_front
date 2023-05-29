30c57320d25702c0c1f1f4b74fb184f8169582ba
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2023-05-29 12:42:04 +0300

    add modalType

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Button, Container } from "react-bootstrap";
+import CreateDevice from "../components/modals/CreateDevice";
+import CreateType from "../components/modals/CreateType";
+import CreateBrand from "../components/modals/CreateBrand";
 
 const Admin = () => {
   return (
-    <Container>
-      <Button>Add type</Button>
-      <Button>Add brand</Button>
-      <Button>Add device</Button>
+    <Container className="d-flex flex-column">
+      <Button variant={"outline-dark"} className="mt-4 p-3">
+        Add type
+      </Button>
+      <Button variant={"outline-dark"} className="mt-4 p-3">
+        Add brand
+      </Button>
+      <Button variant={"outline-dark"} className="mt-4 p-3">
+        Add device
+      </Button>
+      <CreateDevice />
+      <CreateType show={true} />
+      <CreateBrand />
     </Container>
   );
 };
